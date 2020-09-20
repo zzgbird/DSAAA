02093ee2ade74e0491a46e54e3169e808b6e1cad
zzgbird <zzg_30th@outlook.com>
2020-09-20 19:02:21 +0800

    search tree modify 20200920

--- Tree/SearchTree.c
+++ Tree/SearchTree.c
@@ -21,8 +21,7 @@ MakeEmpty( SearchTree T )
     return NULL;
 }
 
-Position
-Find( ElementType X, SearchTree T )
+Position Find( ElementType X, SearchTree T )
 {
     if ( T == NULL )
         return NULL;
@@ -57,16 +56,53 @@ SearchTree Insert( ElementType X, SearchTree T )
     if ( T == NULL )
     {
         /* create and return a one-node tree */
+        T = malloc( sizeof( struct TreeNode ) );
         if ( T == NULL )
+        {
             printf("out of space!!!");
             eixt(-1);
+        }
         else
         {
-
-        }   
+            T->Element = X;
+            T->Left = T->Right = NULL;
+        }
     }
-
+    else if (X < T->Element)
+        T->Left = Insert( X, T->Left );
+    else if ( X > T->Right )
+        T->Right = Inset( X, T->Right );
+    /* Else X is in the tree already; we'll do nothing */
+    return T;  /* Do not forget this line!! */
 }
 
+SearchTree Delete( ElementType X, SearchTree T )
+{
+    Position TmpCell;
+
+    if ( T == NULL )
+        Error("Element not found!");
+    else if ( X < T->Element )
+        T->Left = Delete( X, T->Left );
+    else if ( X > T->Element )
+        T->Right = Delete( X, T->Right );
+    else if ( T->Left && T->Right )
+    {
+        /* Replace wiht smallest in right subtree */
+        TmpCell = FindMin(T->Right);
+        T->Element = TmpCell->Element;
+        T->Right = Delete(TmpCell->Element, T->Right);
+    }
+    else   /* One or zero children */
+    {
+        TmpCell = T;
+        if ( T->Left == NULL)
+            T = T->Right;
+        else if ( T->Right == NULL )
+            T = T->Left;
+        free(TmpCell);
+    }
+    return T;
+}
 
 
